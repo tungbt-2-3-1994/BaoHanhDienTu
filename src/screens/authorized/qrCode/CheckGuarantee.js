e9e0b53acea8e1e79d260439e7afee50a04eddec
tungbt <jsmile1994@gmail.com>
2018-06-10 14:59:53 +0700

    add picker

--- src/screens/authorized/qrCode/CheckGuarantee.js
+++ src/screens/authorized/qrCode/CheckGuarantee.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, SafeAreaView, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { View, Text, Keyboard, KeyboardType, StyleSheet, Modal, Image, ScrollView, SafeAreaView, TouchableOpacity, KeyboardAvoidingView, TouchableHighlight } from 'react-native';
 
 import BackHeader from '../../../components/BackHeader';
 import { width, height } from '../../../constants/dimensions';
@@ -10,6 +10,27 @@ import { priColor } from '../../../constants/colors';
 import { Icon, Button } from 'native-base';
 import IconFont from 'react-native-vector-icons/FontAwesome';
 import { Input } from 'react-native-elements';
+
+import ModalBox from 'react-native-modalbox';
+
+const pickerValues = [
+    {
+        title: 'Ngành 1',
+        value: 'nganh1'
+    },
+    {
+        title: 'Ngành 2',
+        value: 'nganh2'
+    },
+    {
+        title: 'Ngành 3',
+        value: 'nganh3'
+    },
+    {
+        title: 'Ngành 4',
+        value: 'nganh4'
+    },
+]
 
 const ListHeader = ({ title }) => {
     return (
@@ -28,7 +49,9 @@ class CheckGuarantee extends Component {
         identified_number: '',
         serial: '',
         category: '',
-        brand: ''
+        brand: '',
+        language: '',
+        toogleDisplay: false
     }
 
     componentDidMount() {
@@ -42,6 +65,10 @@ class CheckGuarantee extends Component {
 
     onSearch = () => {
         alert('asasas');
+    }
+
+    setPickerValue = (value) => {
+        this.setState({ category: value });
     }
 
     render() {
@@ -58,7 +85,6 @@ class CheckGuarantee extends Component {
                             inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
                             leftIcon={<Icon name='logo-whatsapp' style={{ fontSize: responsiveFontSize(2.5), color: '#969696' }} />}
                             placeholder='Số điện thoại'
-                            errorStyle={{ color: 'red' }}
                             placeholderTextColor='#969696'
                             underlineColorAndroid='transparent'
                             returnKeyType='next'
@@ -71,7 +97,6 @@ class CheckGuarantee extends Component {
                             inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
                             leftIcon={<IconFont name='address-card' style={{ fontSize: responsiveFontSize(1.8), color: '#969696' }} />}
                             placeholder='CMND'
-                            errorStyle={{ color: 'red' }}
                             placeholderTextColor='#969696'
                             underlineColorAndroid='transparent'
                             returnKeyType='next'
@@ -85,7 +110,6 @@ class CheckGuarantee extends Component {
                             inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
                             leftIcon={<Icon name='ios-mail' style={{ fontSize: responsiveFontSize(2.5), color: '#969696' }} />}
                             placeholder='Gmail'
-                            errorStyle={{ color: 'red' }}
                             placeholderTextColor='#969696'
                             underlineColorAndroid='transparent'
                             returnKeyType='next'
@@ -100,35 +124,41 @@ class CheckGuarantee extends Component {
                             inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
                             leftIcon={<IconFont name='barcode' style={{ fontSize: responsiveFontSize(1.8), color: '#969696' }} />}
                             placeholder='Serial'
-                            errorStyle={{ color: 'red' }}
                             placeholderTextColor='#969696'
                             underlineColorAndroid='transparent'
                             returnKeyType='next'
                             ref={(input) => this.serial = input}
-                            onSubmitEditing={() => this.category.focus()}
                             onChangeText={(text) => this.setState({ serial: text })}
                         />
-                        <Input
-                            containerStyle={{ width: '100%', backgroundColor: 'white', marginBottom: 20, padding: 5 }}
-                            inputContainerStyle={{ borderColor: 'white' }}
-                            inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
-                            leftIcon={<IconFont name='shopping-cart' style={{ fontSize: responsiveFontSize(2), color: '#969696' }} />}
-                            placeholder='Chọn ngành hàng'
-                            errorStyle={{ color: 'red' }}
-                            placeholderTextColor='#969696'
-                            underlineColorAndroid='transparent'
-                            returnKeyType='next'
-                            ref={(input) => this.category = input}
-                            onSubmitEditing={() => this.brand.focus()}
-                            onChangeText={(text) => this.setState({ category: text })}
-                        />
+
+                        <TouchableOpacity style={{ flexDirection: 'row', width: '100%', paddingLeft: 18, paddingRight: 8, paddingVertical: 15, marginBottom: 20, justifyContent: 'space-between', backgroundColor: 'white' }} onPress={() => this.refs.modal.open()}>
+                            {/* <Input
+                                containerStyle={{ width: '100%', backgroundColor: 'white', marginBottom: 20, padding: 5 }}
+                                inputContainerStyle={{ borderColor: 'white' }}
+                                inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
+                                leftIcon={<IconFont name='shopping-cart' style={{ fontSize: responsiveFontSize(2), color: '#969696' }} />}
+                                rightIcon={<IconFont name='chevron-down' style={{ marginRight: 10, fontSize: responsiveFontSize(2), color: '#969696' }} />}
+                                placeholder='Chọn ngành hàng'
+                                placeholderTextColor='#969696'
+                                underlineColorAndroid='transparent'
+                                returnKeyType='next'
+                                ref={(input) => this.category = input}
+                                onSubmitEditing={() => this.brand.focus()}
+                                onChangeText={(text) => this.setState({ category: text })}
+                            >
+                            </Input> */}
+                            <View style={{ flexDirection: 'row', }}>
+                                <IconFont name='shopping-cart' style={{ marginRight: 10, fontSize: responsiveFontSize(2), color: '#969696' }} />
+                                <Text style={{ color: '#969696', fontSize: responsiveFontSize(1.8) }}>{this.state.category === '' ? 'Chọn ngành hàng' : this.state.category}</Text>
+                            </View>
+                            <IconFont name='chevron-down' style={{ marginRight: 10, fontSize: responsiveFontSize(2), color: '#969696' }} />
+                        </TouchableOpacity>
                         <Input
                             containerStyle={{ width: '100%', backgroundColor: 'white', marginBottom: 20, padding: 5 }}
                             inputContainerStyle={{ borderColor: 'white' }}
                             inputStyle={{ color: 'black', fontSize: responsiveFontSize(1.8) }}
                             leftIcon={<IconFont name='registered' style={{ fontSize: responsiveFontSize(2), color: '#969696' }} />}
                             placeholder='Thương hiệu'
-                            errorStyle={{ color: 'red' }}
                             placeholderTextColor='#969696'
                             underlineColorAndroid='transparent'
                             returnKeyType='next'
@@ -140,7 +170,18 @@ class CheckGuarantee extends Component {
                         </TouchableOpacity>
                     </ScrollView>
                 </KeyboardAvoidingView>
-            </View>
+                <ModalBox ref={'modal'} swipeToClose={false} style={[styles.modal,]} backdrop={true} position={"bottom"} >
+                    <ScrollView>
+                        {pickerValues.map((value, index) => {
+                            return (
+                                <TouchableHighlight style={{ paddingVertical: 4, alignItems: 'center' }} key={index} onPress={() => this.setPickerValue(value.title)}>
+                                    <Text>{value.title}</Text>
+                                </TouchableHighlight>
+                            );
+                        })}
+                    </ScrollView>
+                </ModalBox>
+            </View >
         );
     }
 }
@@ -150,6 +191,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
+    },
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 200,
+        backgroundColor: "#efefef"
     },
 });
 
